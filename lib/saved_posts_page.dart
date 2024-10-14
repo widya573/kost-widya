6fbe448769ddc06f55a970f146b31707c7271b42
widya setiani <115101750+widya573@users.noreply.github.com>
2024-10-14 19:54:12 +0700

    Update saved_posts_page.dart

--- lib/saved_posts_page.dart
+++ lib/saved_posts_page.dart
@@ -1,12 +1,52 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class SavedPostsPage extends StatelessWidget {
-  const SavedPostsPage({super.key});
+  final List<Map<String, String>> savedPosts;
+  final Function(Map<String, String>) onRemoveSavedKost;
+
+  const SavedPostsPage({
+    super.key,
+    required this.savedPosts,
+    required this.onRemoveSavedKost,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Saved Posts Page: Daftar Kost Tersimpan'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Saved Posts'),
+        backgroundColor: Colors.orange,
+      ),
+      body: savedPosts.isEmpty
+          ? const Center(child: Text('Tidak ada kost yang disimpan.'))
+          : ListView.builder(
+              itemCount: savedPosts.length,
+              itemBuilder: (context, index) {
+                final kost = savedPosts[index];
+                return Card(
+                  margin: const EdgeInsets.all(10),
+                  child: ListTile(
+                    title: Text(kost['namaKost'] ?? 'Nama Kost'),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Alamat: ${kost['alamatKost']}'),
+                        Text('Nomor HP: ${kost['nomorHP']}'),
+                      ],
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () => onRemoveSavedKost(kost), // Menghapus kost dari saved posts
+                    ),
+                    leading: kost['imagePath'] == '' 
+                        ? const Icon(Icons.image_not_supported) 
+                        : Image.file(File(kost['imagePath']!)),
+                  ),
+                );
+              },
+            ),
     );
   }
 }
